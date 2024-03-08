b2af51a7f78028b101a19801601a2d2923c23d56
anshuman.a <anshuman.a@perfios.com>
2024-03-08 22:28:52 +0530

    print on console

--- run_main.py
+++ run_main.py
@@ -27,26 +27,26 @@ def get_spark_session(spark_conf=None):
 if __name__ == '__main__':
     # conf = ConfigFactory.parse_file('application.conf')
 
-    # spark_conf = conf['spark']
-    spark, sc = get_spark_session()
-
-    print("Created spark session")
-
-    # kafka_conf = conf['kafka']
-    kafka_df = get_df_from_kafka(spark=spark)
-
-    print("connected to kafka")
-
-    # schema = read_schema('schema.json')
-    # parsed_df = get_df_from_schema(kafka_df, schema)
-
-    query = print_df_on_console(kafka_df)
-
-    query.start()
-
-    # hbase_connection = dao.HBaseDao()
+    # # spark_conf = conf['spark']
+    # spark, sc = get_spark_session()
     #
-    # rows = hbase_connection.get_data(key=b'6544649161377464', table='lookup')
-    # print(rows[b'bt:score'])
+    # print("Created spark session")
+    #
+    # # kafka_conf = conf['kafka']
+    # kafka_df = get_df_from_kafka(spark=spark)
+    #
+    # print("connected to kafka")
+    #
+    # # schema = read_schema('schema.json')
+    # # parsed_df = get_df_from_schema(kafka_df, schema)
+    #
+    # query = print_df_on_console(kafka_df)
+    #
+    # query.start()
+
+    hbase_connection = dao.HBaseDao()
+
+    rows = hbase_connection.get_data(key=b'6544649161377464', table='lookup')
+    print(rows[b'bt:score'])
 
     # sc.broadcast(hbase_connection)
